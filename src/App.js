223f0a149d1576ce4e52f8c55895c838afc4ba89
yashh-dev <yashwanthkrishna5027@gmail.com>
2022-12-19 21:03:49 +0530

    context integrated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Favorites from './components/Favorites'
+import Meals from './components/Meals'
+import Modal from './components/Modal'
+import Search from './components/Search'
+import { useGlobalContext } from './context'
 
-function App() {
+export default function App() {
+  const {showModal} = useGlobalContext();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Search/>
+      {/*<Favorites/>*/}
+      <Meals className='container-fluid' />
+      {showModal && <Modal />}
+    </main>
+  )
 }
 
-export default App;
